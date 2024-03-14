d9908617d0f45bb98fa7c2e521999d92f6ffea1c
atryomkorshykau <butovichjunior@gmail.com>
2024-03-14 19:40:49 +0300

    add: utilities functions

---
+++ src/utils/useAppDispatch.ts
@@ -0,0 +1,4 @@
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/types/app/type'
+
+export const useAppDispatch: () => AppDispatch = useDispatch
---
+++ src/utils/useAppSelector.ts
@@ -0,0 +1,4 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import { AppRootState } from '@/types/app/type'
+
+export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
